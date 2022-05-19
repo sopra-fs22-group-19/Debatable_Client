fc6601431607e7b227b433f5f5b2d280fc559bbc
Rupal Saxena <rupal.saxena@uzh.ch>
2022-05-19 16:18:29 +0200

    in progress profile page

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
 import {Button} from 'components/ui/Button';
+//import BaseContainer from "components/ui/BaseContainer";
 import 'styles/ui/BaseContainer.scss';
-import 'styles/views/Login_Trial.css';
+//import 'styles/views/Login_Trial.css';
+import 'styles/views/Profile.scss';
 import { Link } from 'react-router-dom';
 import Header from "./Header";
 
@@ -43,11 +45,8 @@ const Profile = () => {
             </div>
         );
     }
-
-    return (
-       <div>
-            <Header height={"100"}/>
-            <div id="card2" className="card2 card border-0 px-4 py-5">
+    /*
+    <div id="card2" className="card2 card border-0 px-4 py-5">
                 <div className="row mb-4 px-3  d-flex justify-content-center">
                     <div className="row px-3">
                         <FormField label="Username:"/>
@@ -64,7 +63,38 @@ const Profile = () => {
                     </Link>
                 </div>
             </div>
-       </div>
+
+            <Button width="50%" onClick={() => doUpdate()}>Save</Button>
+    */
+
+    return (
+            <div>
+                <Header height={"100"}/>
+                <div className='container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto'></div>
+                <div id="card2" className="border-0 px-4 py-5  d-flex justify-content-center" style={{"background-color":"white", "width":"40%"}}>
+                    <div className="row mb-4 px-3 d-flex justify-content-center">
+                        <div className="row px-3 mb-2" style={{"color":"green", "font-size": "x-large" }}>
+                            Username: {username}
+                        </div>
+
+                        <div className="row px-3 mb-2" style={{"color":"green", "font-size": "x-large" }}>
+                            Name: {name}
+                        </div>
+
+                        <div className="row px-3 mb-5" style={{"color":"green", "font-size": "x-large" }}>
+                            Password: *****
+                        </div>
+
+                        <div className="row px-3  d-flex justify-content-center">
+                            <Link to ={`/edit/${userId}`}>
+                                <Button width="40%">Edit Profile</Button>
+                            </Link>
+                        </div>
+
+                    </div>  
+                </div>
+            </div>
+       
     );
 }
 
