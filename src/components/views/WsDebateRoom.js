ceb25f0247c82e288194ab3e6d9da7004b2ba4f1
oroikono <orestis.oikonomou@uzh.ch>
2022-05-20 15:38:27 +0200

    guest name

--- src/components/views/WsDebateRoom.js
+++ src/components/views/WsDebateRoom.js
@@ -104,7 +104,8 @@ const DebateRoom = () => {
             'isInvitedSide': !isStartingSide,
         }));
 
-        setAdvocatingUser(prevAdvocatingUser => ({...prevAdvocatingUser,
+        setAdvocatingUser(prevAdvocatingUser => (
+            {...prevAdvocatingUser,
             'id': advocatingUserId,
             'userName': advocatingUserName,
             'side': advocatingUserSide
@@ -130,16 +131,16 @@ const DebateRoom = () => {
             if (parseInt(userId) === debateRoom.user1.userId) {
                 if (debateRoom.user2 === null){
                     setAllUserStates(
-                        true, debateRoom.user1.userId, debateRoom.user1.username, debateRoom.side1,
+                        true, debateRoom.user1.userId, debateRoom.user1.name, debateRoom.side1,
                         null, null);
-                    return {userName: debateRoom.user1.username, isInvitee: false,
+                    return {userName: debateRoom.user1.name, isInvitee: false,
                         advocatingUser: {id: debateRoom.user1.userId, side: debateRoom.side1},
                         opponentUser: null};
                 } else {
                     setAllUserStates(
-                        true, debateRoom.user1.userId, debateRoom.user1.username, debateRoom.side1,
-                        debateRoom.user2.username, debateRoom.user2.userId);
-                    return {userName: debateRoom.user1.username, isInvitee: false,
+                        true, debateRoom.user1.userId, debateRoom.user1.name, debateRoom.side1,
+                        debateRoom.user2.name, debateRoom.user2.userId);
+                    return {userName: debateRoom.user1.name, isInvitee: false,
                         advocatingUser: {id: debateRoom.user1.userId, side: debateRoom.side1},
                         opponentUser: {id: debateRoom.user2.userId, side: debateRoom.side2}};
                 }
@@ -156,10 +157,10 @@ const DebateRoom = () => {
             //  then add retrieve their information in the debate
             if (parseInt(userId) === debateRoom.user2.userId) {
                 setAllUserStates(
-                    false, debateRoom.user2.userId, debateRoom.user2.username, debateRoom.side2,
-                    debateRoom.user1.username, debateRoom.user1.userId);
+                    false, debateRoom.user2.userId, debateRoom.user2.name, debateRoom.side2,
+                    debateRoom.user1.name, debateRoom.user1.userId);
 
-                return {userName: debateRoom.user2.username, isInvitee: true,
+                return {userName: debateRoom.user2.name, isInvitee: true,
                     advocatingUser: {id: debateRoom.user2.userId, side: debateRoom.side2},
                     opponentUser: {id: debateRoom.user1.userId, side: debateRoom.side1}};
             }
@@ -168,8 +169,8 @@ const DebateRoom = () => {
         // If they are an Observer
         if (debateRoom.user1 && debateRoom.user2) {
             if (parseInt(userId) !== debateRoom.user1.userId && parseInt(userId) !== debateRoom.user2.userId) {
-                setAllUserStates(false, debateRoom.user2.userId, debateRoom.user2.username, debateRoom.side2,
-                    debateRoom.user1.username, debateRoom.user1.userId);
+                setAllUserStates(false, debateRoom.user2.userId, debateRoom.user2.name, debateRoom.side2,
+                    debateRoom.user1.name, debateRoom.user1.userId);
 
                 setUserState(prevUserState => ({
                     ...prevUserState,
@@ -205,20 +206,20 @@ const DebateRoom = () => {
             if (userState.isStartingSide && parseInt(userId) === response.data.user1.userId ){
                 setOpponentUser(prevOpponentUser => ({...prevOpponentUser,
                     'id': response.data.user2.userId,
-                    'userName': response.data.user2.username
+                    'userName': response.data.user2.name
                 }))
             } else if (userState.isInvitedSide && parseInt(userId) === response.data.user2.userId ){
                 setOpponentUser(prevOpponentUser => ({...prevOpponentUser,
                     'id': response.data.user1.userId,
-                    'userName': response.data.user1.username
+                    'userName': response.data.user1.name
                 }))
             } else{
                 setAdvocatingUser(prevUserState => ({...prevUserState,
-                    'userName': response.data.user1.username,
+                    'userName': response.data.user1.name,
                 }));
                 setOpponentUser(prevOpponentUser => ({...prevOpponentUser,
                     'id': response.data.user2.userId,
-                    'userName': response.data.user2.username
+                    'userName': response.data.user2.name
                 }))
             }
 
